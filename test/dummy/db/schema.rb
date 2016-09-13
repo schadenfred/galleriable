6ad4f41a316f85e79d10f2318d0f84d5bd058442
schadenfred <fred.schoeneman@gmail.com>
2016-09-12 21:31:40 -0700

    added galleries

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913003004) do
+ActiveRecord::Schema.define(version: 20160913042947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleriable_galleries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "galleriable_type"
+    t.integer  "galleriable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["galleriable_type", "galleriable_id"], name: "by_galleriable", using: :btree
+  end
 
   create_table "galleriable_photos", force: :cascade do |t|
     t.string   "image_uid"
